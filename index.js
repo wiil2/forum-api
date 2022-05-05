f380f10a5581bdb71204618e3880e9a5d1a3bbd2
Alexandre Lima <alexandreatlima@gmail.com>
2022-05-05 16:51:11 -0300

    crud do user quase done

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 
 app.use(express.json());
 
+const userRouter = require("./routes/user.routes");
+app.use("/user", userRouter);
+
 app.listen(Number(process.env.PORT), () => {
   console.log("Server up and running at port:", process.env.PORT);
 });
---
+++ routes/user.routes.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const UserModel = require("../models/User.model");
+
+// "C"RUD
+
+router.post("/signup", async (req, res) => {
+  try {
+    const createdUser = await UserModel.create(req.body);
+
+    return res.status(201).json(createdUser);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+// C"R"UD
+
+router.get("/profile/:userId", async (req, res) => {
+  try {
+    const foundedUser = await UserModel.findOne({ _id: req.params.userId });
+    return res.status(200).json(foundedUser);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+// CR"U"D
+
+router.patch("/update-profile/:userId", async (req, res) => {
+  try {
+    const updatedUser = await UserModel.findOneAndUpdate(
+      { _id: req.params.userId },
+      { ...req.body },
+      { runValidators: true, new: true }
+    );
+
+    return res.status(200).json(updatedUser);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+module.exports = router;
